08d397786156dae0d33cbfaf6189d845ed480a5a
Yashodip Beldar <yashodip2003beldar@gmail.com>
2023-11-16 20:37:16 +0530

    created login  page which render on login component

---
+++ src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Login = ({setIsLoggedIn}) => {
+  return (
+    <div class="flex flex-1 justify-center items-center text-white text-3xl">Login
+    </div>
+  )
+}
+
+export default Login
