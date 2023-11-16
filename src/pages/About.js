6c10d9a76e08191c5337bc4b456560c44e7bcc83
Yashodip Beldar <yashodip2003beldar@gmail.com>
2023-11-16 20:35:48 +0530

    created about balck page

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div class="flex flex-1 justify-center items-center text-white text-3xl">About</div>
+  )
+}
+
+export default About
